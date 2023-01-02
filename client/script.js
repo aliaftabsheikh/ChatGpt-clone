edaad3870e4a21836ecb348852bab746ca178e9a
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-01-02 11:29:05 +0500

    feature: submit message

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
-const chat_container = document.querySelector("#chat_container");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
@@ -46,7 +46,7 @@ function chatStripe(isAi, value, uniqueId) {
          <div class="profile">
            <img
              src="${isAi ? bot : user}"
-             alt="${isAi ? 'bot' : 'user'}"
+             alt="${isAi ? "bot" : "user"}"
             />
         </div>
 
@@ -57,3 +57,34 @@ function chatStripe(isAi, value, uniqueId) {
     </div>
     `;
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //users chatStripe
+
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  //bots chatStripe
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, "", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e)=>{
+  if(e.keyCode === 13){
+    handleSubmit(e)
+  }
+})
